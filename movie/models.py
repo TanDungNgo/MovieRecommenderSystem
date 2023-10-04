1b74f71eb23d42cedd5bc2a83278746cded2391d
Phương Trần Khánh <102200314@sv1.dut.udn.vn>
2023-10-04 10:17:55 +0700

    fix_models

--- movie/models.py
+++ movie/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     poster = models.ImageField(upload_to='img/')
