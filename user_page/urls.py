ddc54001c93ab93502919b7affcde28f9cbd0623
TrangLe <letrangle2002@gmail.com>
2023-10-05 23:07:37 +0700

    fix avatar header

--- user_page/urls.py
+++ user_page/urls.py
@@ -4,4 +4,5 @@ from . import views  # Thay thế bằng tên module và view của bạn
 urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
+    path('signout/', views.signout, name='signout'),
 ]
