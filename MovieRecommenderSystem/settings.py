59ecf38c4a43aae123b6f4bfe4c0ecf0d5d9f776
Tan Dung <tandung12072002@gmail.com>
2023-10-05 21:01:01 +0700

    edit database

--- MovieRecommenderSystem/settings.py
+++ MovieRecommenderSystem/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'MovieRecommenderSystem.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movie_recommender_system',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost', 
+        'PORT': '3306',       
     }
 }
 
