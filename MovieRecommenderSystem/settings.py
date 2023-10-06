b2b81565d4b2cb333a32263253adc0c31e7dbd89
TrangLe <letrangle2002@gmail.com>
2023-10-06 15:27:09 +0700

    middlewares

--- MovieRecommenderSystem/settings.py
+++ MovieRecommenderSystem/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'user_page.middlewares.AdminAccessMiddleware',
 ]
 
 ROOT_URLCONF = 'MovieRecommenderSystem.urls'
---
+++ user_page/middlewares.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+
+class AdminAccessMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Kiểm tra xem người dùng có session 'user_role' không
+        if 'user_role' in request.session:
+            user_role = request.session['user_role']
+            # Kiểm tra xem vai trò của người dùng có phải là admin không
+            if user_role != 'admin':
+                # Chặn người dùng truy cập vào các đường dẫn chỉ dành cho admin
+                if request.path.startswith('/admin/'):
+                    return redirect('/signin/')
+        
+        response = self.get_response(request)
+        return response
