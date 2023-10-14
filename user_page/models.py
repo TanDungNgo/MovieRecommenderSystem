b283857ca2b5e9dc5101ee7f73ac6485d74f1445
TrangLe <letrangle2002@gmail.com>
2023-10-14 22:13:09 +0700

    fix

--- user_page/models.py
+++ user_page/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class MyUser(models.Model):
     username = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=128)
     avatar = models.ImageField(upload_to='img/')
